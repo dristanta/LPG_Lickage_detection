69a715cc52863dd7d5e9ae3d04a7eb4510173949
dristanta <35772232+dristanta@users.noreply.github.com>
2018-10-02 15:51:03 +0530

    Add files via upload

---
+++ LPG_lickage.ino
@@ -0,0 +1,24 @@
+ #include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+void setup()
+{
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  delay(100);
+}
+
+
+void loop()
+{
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      SendMessage();
+      break;
+    case 'r':
+      RecieveMessage();
+      break;
+  }
